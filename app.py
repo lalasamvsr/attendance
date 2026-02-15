5b2579e089f9ae94543ee300c1b3bd840e62d370
lalasamvsr <245123733069@mvsrec.edu.in>
2026-02-15 10:24:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -431,7 +431,13 @@ def week_report():
     schedule_id = request.args.get('schedule_id', type=int)
 
     if not selected_date or not schedule_id:
-        return render_template("week_report.html", report=None)
+    return render_template(
+        "week_report.html",
+        report=None,
+        schedule_id=schedule_id,
+        selected_date=selected_date
+    )
+
 
     report_date = datetime.strptime(selected_date, "%Y-%m-%d").date()
 
@@ -987,6 +993,7 @@ def logout():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
 
 
 
